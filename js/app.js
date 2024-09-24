bbbfea05280cde62d2959f16e19b489092a9ed6c
hussainshamim16 <hj422748@gmail.com>
2024-09-24 04:45:31 -0700

    animation on scrolling

--- js/app.js
+++ js/app.js
@@ -36,3 +36,32 @@
 // });
 
 console.log("hello world")
+
+
+AOS.init({
+    offset: 140, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 800, // values from 0 to 3000, with step 50ms
+    easing: 'ease'
+  })
+
+let firstNm = 420;
+let secondNm = 0;
+let thirdNm = 220;
+
+
+
+secondNm++
+thirdNm++
+const counter = setInterval(()=>{
+  if(firstNm < 482){
+    let firstReder = document.getElementById('firstReder').innerHTML = '+' + firstNm++
+    let secondReder = document.getElementById('secondReder').innerHTML = '+' + secondNm++
+    let thirdReder = document.getElementById('thirdReder').innerHTML = '+' + thirdNm++
+  }
+  else{
+    clearInterval(counter)
+  }
+  
+  console.log(firstNm)
+  },100)
