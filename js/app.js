54868fd3ab1be35573344da4f7bc2f682459cc2a
hussainshamim16 <hj422748@gmail.com>
2024-10-20 09:06:31 -0700

    updations

--- js/app.js
+++ js/app.js
@@ -39,9 +39,9 @@ console.log("hello world")
 
 
 AOS.init({
-    offset: 50, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 800, // values from 0 to 3000, with step 50ms
+    offset: 35,
+    delay: 5, 
+    duration: 800, 
     easing: 'ease'
   })
 
